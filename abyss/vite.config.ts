3754a91615c60b83d1e535d82d3888970984715d
aragon <e.g.aragon2015@gmail.com>
2023-05-01 09:56:30 -0500

    added extensions to vite.config.ts

--- abyss/vite.config.ts
+++ abyss/vite.config.ts
@@ -6,10 +6,22 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    }, 
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
   }
 })
