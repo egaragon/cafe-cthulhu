3da68f0603745c6ba5032206417f382ace0e0996
aragon <e.g.aragon2015@gmail.com>
2023-05-05 03:54:02 -0500

    logo changes!

--- abyss/src/App.vue
+++ abyss/src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="cute cthulhu" class="logo" src="/src/assets/cutethulhu.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,6 +18,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
+
 header {
   line-height: 1.5;
 }
